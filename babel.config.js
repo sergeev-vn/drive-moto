a1e011aaedf8c8d81bab4f79d65669c653f75bf6
Vladimir Sergeev <sergeev24085@mail.ru>
2020-08-04 15:46:22 +0700

    config files

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+module.exports = function(api) {
+  api.cache(true);
+
+  return {
+    presets: [
+      '@babel/preset-env'
+    ],
+    plugins: [
+      ['@babel/plugin-proposal-optional-chaining'],
+      ['@babel/plugin-syntax-dynamic-import']
+    ]
+  };
+};
---
+++ webpack.config.js
@@ -0,0 +1,135 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const SpriteLoaderPlugin = require("svg-sprite-loader/plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = (env, argv) => {
+  const isProductionBuild = argv.mode === "production";
+  const publicPath = '';
+
+  const pcss = {
+    test: /\.(p|post|)css$/,
+    use: [
+      isProductionBuild
+        ? MiniCssExtractPlugin.loader
+        : "style-loader", "css-loader", "postcss-loader"
+    ]
+  };
+
+  const js = {
+    test: /\.js$/,
+    loader: "babel-loader",
+    exclude: /node_modules/,
+  };
+
+  const files = {
+    test: /\.(png|jpe?g|gif|woff2|woff|ttf|ico?)$/i,
+    loader: "file-loader",
+    options: {
+      name: "[hash].[ext]"
+    }
+  };
+
+  const svg = {
+    test: /\.svg$/,
+    use: [
+      {
+        loader: "svg-sprite-loader",
+        options: {
+          extract: true,
+          spriteFilename: svgPath => `sprite${svgPath.substr(-4)}`
+        }
+      },
+      "svg-transform-loader",
+      {
+        loader: "svgo-loader",
+        options: {
+          plugins: [
+            { removeTitle: true },
+            {
+              removeAttrs: {
+                attrs: "(fill|stroke)"
+              }
+            }
+          ]
+        }
+      }
+    ]
+  };
+
+  const pug = {
+    test: /\.pug$/,
+    loader: ["pug-loader"]
+  };
+
+  const config = {
+    entry: {
+      main: "./src/main.js"
+    },
+    output: {
+      path: path.resolve(__dirname, "./dist"),
+      filename: "[name].[hash].build.js",
+      publicPath: isProductionBuild ? publicPath : "",
+      chunkFilename: "[chunkhash].js"
+    },
+    module: {
+      rules: [pcss, js, files, svg, pug]
+    },
+    resolve: {
+      alias: {
+        images: path.resolve(__dirname, "src/images"),
+      },
+      extensions: ["*", ".js"]
+    },
+    devServer: {
+      noInfo: false,
+      overlay: true
+    },
+    performance: {
+      hints: false
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "src/index.pug",
+        chunks: ["main"]
+      }),
+      new SpriteLoaderPlugin({ plainSprite: true }),
+      new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery"
+    })
+    ],
+    devtool: "#eval-source-map"
+  };
+
+  if (isProductionBuild) {
+    config.devtool = "none";
+    config.plugins = (config.plugins || []).concat([
+      new webpack.DefinePlugin({
+        "process.env": {
+          NODE_ENV: '"production"'
+        }
+      }),
+      new MiniCssExtractPlugin({
+        filename: "[name].[contenthash].css",
+        chunkFilename: "[contenthash].css"
+      })
+    ]);
+
+    config.optimization = {};
+
+    config.optimization.minimizer = [
+      new TerserPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: false
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ];
+  }
+
+  return config;
+};
