c2a44b5392c281e012838226c9622481e0ef064d
Vladimir <sergeev24085@mail.ru>
2020-08-18 23:44:36 +0700

    filter form last blocks

--- src/scripts/inputs-styler.js
+++ src/scripts/inputs-styler.js
@@ -2,3 +2,4 @@ $('.filter-dropdown__checkbox').styler();
 $('.sorting-order').styler();
 $('.filter-dropdown__radio').styler();
 $('.filter-select__select').styler();
+$('.filter-dropdown__input').styler();
