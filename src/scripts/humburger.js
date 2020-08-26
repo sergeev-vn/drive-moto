4112d4ac9aa26cff7ad2a278897d0630a46df697
Vladimir Sergeev <sergeev24085@mail.ru>
2020-08-26 12:35:42 +0700

    finished adptive header

--- src/scripts/humburger.js
+++ src/scripts/humburger.js
@@ -1,3 +1,5 @@
 $('.humburger-button').on('click', function() {
-  $('.menu-mob').toggleClass('menu-mob_opened')
+  $('.menu-mob__list').toggleClass('menu-mob__list_opened')
+  $('.menu-mob__line').toggleClass('menu-mob__line_hide')
+  $(this).toggleClass('active')
 });
