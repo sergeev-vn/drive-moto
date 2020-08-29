83535e48a30fc7aef5725156277b72fd39ca2880
Vladimir <sergeev24085@mail.ru>
2020-08-29 23:30:37 +0700

    adaptive footer

--- src/scripts/dropdown.js
+++ src/scripts/dropdown.js
@@ -2,3 +2,10 @@ $('.filter-dropdown__title').on('click', function() {
   $(this).toggleClass('filter-dropdown__title_active');
   $(this).next().slideToggle('fast');
 })
+
+$('.footer__top-title').on('click', function() {
+  if (window.innerWidth <= '320') {
+    $(this).next().slideToggle('fast');
+    $(this).toggleClass('footer__top-title_active');
+  }
+})
