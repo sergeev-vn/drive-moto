33d16be5533d91e56684ec6a68d28b6e8699dc46
Vladimir <sergeev24085@mail.ru>
2020-08-29 17:01:18 +0700

    product-item adaptive

--- src/scripts/product-slider.js
+++ src/scripts/product-slider.js
@@ -6,5 +6,42 @@ $('.products-slider__list').each(function() {
     slidesToScroll: 1,
     nextArrow: $(this).closest('.products-slider').find('.arrow_next'),
     prevArrow:  $(this).closest('.products-slider').find('.arrow_prev'),
+    responsive: [
+      {
+        breakpoint: 1320,
+        settings: {
+          arrows: false,
+          dots: true,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 1176,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          dots: true,
+          arrows: false,
+        }
+      },
+      {
+        breakpoint: 888,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          dots: true,
+          arrows: false,
+        }
+      },
+      {
+        breakpoint: 506,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true,
+          arrows: false,
+        }
+      }
+    ]
   });
 });
